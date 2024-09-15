c9bd0a326761889570b7edb6d616dbe5d3617551
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2024-09-15 18:33:49 +0200

    log

--- app/routes/blog.$slug.tsx
+++ app/routes/blog.$slug.tsx
@@ -13,7 +13,10 @@ import { RootLoaderType } from '~/root';
 export async function loader({ params }: LoaderFunctionArgs) {
 	const __dirname = process.cwd();
 	const { slug } = params;
-	const filePath = path.join(__dirname, `./client/content/${slug}.mdx`);
+
+	console.log(__dirname, 'HEY');
+
+	const filePath = path.join(__dirname, `../client/content/${slug}.mdx`);
 	const fileContent = await fs.readFile(filePath, 'utf-8');
 
 	const { code, frontmatter } = await bundleMDX({
