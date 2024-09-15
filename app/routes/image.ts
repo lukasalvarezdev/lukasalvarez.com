7363a2f4d754b079fc26fc28a3b820067d18138c
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2024-09-15 19:00:38 +0200

    it migth not

--- app/routes/image.ts
+++ app/routes/image.ts
@@ -2,8 +2,16 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export async function loader() {
-	const filePath = path.join(process.cwd(), 'public', 'memoji.png');
-	const fileContent = await fs.readFile(filePath, 'utf-8');
-	console.log(fileContent);
-	return { fileContent };
+	await readFile('mememoji.png');
+	await readFile('software-as-an-art.mdx');
+	return { hi: 'World' };
+}
+
+async function readFile(rest: string) {
+	try {
+		const filePath = path.join(process.cwd(), 'public', rest);
+		return await fs.readFile(filePath, 'utf-8');
+	} catch (error) {
+		console.log(error);
+	}
 }
